33ba59dea17c0235e4f98944144cde498d3f663e
Sruthy S Kumar <sruthyskumar@mca.ajce.in>
2020-04-22 16:17:59 +0530

    admin dashboard updated

--- admin/index.php
+++ admin/index.php
@@ -1,23 +1,20 @@
 <?php
 include_once ('../autoload.php');
+
+validate_user('admin');
+
 $t = new TemplateEngine('admin'); 
 $t->data = [] ; 
 
-
 $page_data['login'] = $login_status;
 $page_data['login_type'] = $login_type;
-
 $page_data['assets'] = $web_assets;
+ 
+$page_data['assets'] = $admin_assets;
 
-$page_data['page_title'] = "Dashboard" ; 
+$page_data['page_title'] = "Dashboard" ;   
 
-$page_data['total_jobs'] =  500 ; 
-$page_data['total_members'] = 1563 ; 
-$page_data['total_resumes'] = 250 ; 
-$page_data['total_companies'] = 80 ; 
-
-$page_data['testimonials'] = [ 1, 2, 3, 4, 5 ] ; 
-
+//print_r($page_data); exit ; 
 
 $t->data= $page_data ; 
 $t->render('inc/header.phtml');  
