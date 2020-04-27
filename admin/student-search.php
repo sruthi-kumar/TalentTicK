7ba8fa15b8707f22392a11f2612d66e93a2652fa
Sruthy S Kumar <sruthyskumar@mca.ajce.in>
2020-04-27 10:58:00 +0530

    admin module update

---
+++ admin/student-search.php
@@ -0,0 +1,19 @@
+<?php
+include_once '../autoload.php';
+validate_user('admin');
+
+$t = new TemplateEngine('admin');
+$t->data = [];
+
+$page_data = get_current_user_set();
+
+$page_data['assets'] = $admin_assets;
+
+$page_data['page'] = "job-list";
+$page_data['page_title'] = "Job List";
+
+$t->data = $page_data;
+$t->render('inc/header.phtml');
+$t->render('inc/nav.phtml');
+$t->render('student-search.phtml');
+$t->render('inc/footer.phtml');
