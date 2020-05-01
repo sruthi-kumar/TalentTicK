2e89e0d84631046e18b94a7f42bf512a4bce1aa5
Sruthy S Kumar <sruthyskumar@mca.ajce.in>
2020-05-02 02:04:12 +0530

    job accpetance notification added

--- recruiter/actions/job-application-process.php
+++ recruiter/actions/job-application-process.php
@@ -17,7 +17,29 @@ if (isset($_GET['id']) && isset($_GET['action'])) {
 	$job_appication->setData('user', $job_appication_details['user']);
 	$job_appication->setData('status', $_GET['action']);
 
-	$job_appication->update($_GET['id']);
+	if ($job_appication->update($_GET['id'])) {
+
+		$user = new User();
+
+		$user_details = $user->getUserById($job_appication_details['user']);
+		debug($user_details);
+
+		$subject = "Your Application has been accepted by Recruiter";
+
+		$body = " Dear Student .<br>  Your Application has been accepted by Recruiter <br> Please check your account <br> ";
+
+		$notification = new Notification();
+
+		$notification->setData('user', $user_details['user_id']);
+		$notification->setData('title', $subject);
+		$notification->setData('description', $body);
+
+		if ($notification->create()) {
+
+			send_email_notification($user_details['email'], $subject, $body);
+
+		}
+	}
 }
 
 header("location:../job-applications.php?status=$status");
