21b14f29c8d230f55c426c7632b9816e068ba254
Sruthy S Kumar <sruthyskumar@mca.ajce.in>
2020-06-03 20:56:33 +0530

    student password update status shown

--- student/update-password.php
+++ student/update-password.php
@@ -10,10 +10,20 @@ $page_data = get_current_user_set();
 $page_data['assets'] = $admin_assets;
 
 $page_data['page'] = "update-password";
-$page_data['page_title'] = "update-password";
+$page_data['page_title'] = "Update Password";
 
-$t->data = $page_data;
-$t->render('inc/header.phtml');
-$t->render('inc/nav.phtml');
-$t->render('update-password.phtml');
-$t->render('inc/footer.phtml');
+if (isset($_GET['status']) && $_GET['status'] == 'success') {
+
+	$t->data = $page_data;
+	$t->render('inc/header.phtml');
+	$t->render('password-updated.phtml');
+	session_destroy();
+	exit;
+} else {
+
+	$t->data = $page_data;
+	$t->render('inc/header.phtml');
+	$t->render('inc/nav.phtml');
+	$t->render('update-password.phtml');
+	$t->render('inc/footer.phtml');
+}
