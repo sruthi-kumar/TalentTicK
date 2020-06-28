17f0c076bb148467d88f7e2ed43be710f09c387a
Sruthy S Kumar <sruthyskumar@mca.ajce.in>
2020-06-28 16:57:34 +0530

    bluck msg to recruitrs adn studetns added

---
+++ admin/actions/bluckmessage-post-action.php
@@ -0,0 +1,82 @@
+<?php
+require_once '../../autoload.php';
+
+validate_user('admin');
+
+$status = 'success';
+
+/*
+
+`bluckmessages` WHERE 1  `id`, `user`, `user_type`, `message`, `status`, `show_in_web`, `created_at`, `updated_at`
+
+ */
+
+function validate_form($form_data) {
+
+	$status = true;
+
+	if (empty($form_data['message'])) {
+		$status = false;
+	}
+
+	return $status;
+}
+
+//debug($_SERVER['HTTP_REFERER']);
+
+//debug($_POST);
+
+if (validate_form($_POST)) {
+
+	$sendergroup = $_POST['sendergroup'];
+	$message = $_POST['message'];
+
+	//debug($message);
+
+	if ($sendergroup == 'recruiters') {
+
+		$recruiter = new Recruiter();
+		$recpients = $recruiter->getRecruiters('list', 'approved');
+
+	}
+	if ($sendergroup == 'students') {
+		$student = new Student();
+
+		$recpients = $student->getStudents();
+	}
+
+	//debug($recpients);
+
+	if ($recpients) {
+
+		foreach ($recpients as $recpient) {
+
+			$subject = "Message From Talentick Admin";
+
+			$notification = new Notification();
+
+			$notification->setData('user', $recpient["user_id"]);
+			$notification->setData('title', $subject);
+			$notification->setData('message', $message);
+
+			if ($notification->create()) {
+
+				send_email_notification($recpient["email"], $subject, $message);
+
+			}
+
+		}
+
+		$_SESSION['bluckmessage']['message'] = "Bluck Message Successfull!";
+
+	} else {
+		$status = 'failed';
+		$_SESSION['errors']['bluckmessage'] = "Bluck Message Failed!";
+	}
+
+} else {
+	$status = 'failed';
+	$_SESSION['errors']['bluckmessage'] = "Invalid Testimonial Data!";
+}
+
+header("location:" . $_SERVER['HTTP_REFERER'] . "?status=$status");
---
+++ admin/recruiter-message.php
@@ -0,0 +1,29 @@
+<?php
+require_once '../autoload.php';
+validate_user('admin');
+
+$t = new TemplateEngine('admin');
+$t->data = [];
+
+$page_data = get_current_user_set();
+
+$page_data['assets'] = $admin_assets;
+
+$page_data['page'] = "recruiter-list";
+$page_data['page_title'] = "Bluck Message to Recruiters";
+
+$page_data['sendergroup'] = "recruiters";
+if (isset($_GET['status'])) {
+
+	if ($_GET['status'] == 'success') {
+		$page_data['op_status'] = '<label style="color: green" > Post Successfull </label> ';
+	}if ($_GET['status'] == 'failed') {
+		$page_data['op_status'] = '<label style="color: red" >Post Failed</label> ';
+	}
+
+}
+$t->data = $page_data;
+$t->render('inc/header.phtml');
+$t->render('inc/nav.phtml');
+$t->render('bluck-message.phtml');
+$t->render('inc/footer.phtml');
