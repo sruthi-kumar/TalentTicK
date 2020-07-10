2cefef7dd845119ef217c8b1619e0a4dbf3136f2
Sruthy S Kumar <sruthyskumar@mca.ajce.in>
2020-07-10 13:12:56 +0530

    admin pass updated msg mising fix

--- admin/update-password.php
+++ admin/update-password.php
@@ -12,8 +12,18 @@ $page_data['assets'] = $admin_assets;
 $page_data['page'] = "update-password";
 $page_data['page_title'] = "Update Password";
 
-$t->data = $page_data;
-$t->render('inc/header.phtml');
-$t->render('inc/nav.phtml');
-$t->render('update-password.phtml');
-$t->render('inc/footer.phtml');
+if (isset($_GET['status']) && $_GET['status'] == 'success') {
+
+	$t->data = $page_data;
+	$t->render('inc/header.phtml');
+	$t->render('password-updated.phtml');
+	session_destroy();
+	exit;
+} else {
+
+	$t->data = $page_data;
+	$t->render('inc/header.phtml');
+	$t->render('inc/nav.phtml');
+	$t->render('update-password.phtml');
+	$t->render('inc/footer.phtml');
+}
