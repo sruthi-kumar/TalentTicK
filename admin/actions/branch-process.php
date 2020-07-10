3ac889d6f1531de586bf6ed09ced6dbb1aeac2e6
Sruthy S Kumar <sruthyskumar@mca.ajce.in>
2020-07-10 15:33:22 +0530

    branch update fixed

--- admin/actions/branch-process.php
+++ admin/actions/branch-process.php
@@ -8,76 +8,61 @@ $status = 'success';
 //debug($_POST);
 
 $branch = new Branch();
-/*
-`branchs` WHERE 1 `id`, `branch`*/
 
+$branch_details = $branch->getBranchByBranchname($_POST['branch'], $_POST['department_id']);
 
+//debug($branch_details);
 
-$department_details = $department->getDepartmentByDepartmentname($_POST['department']);
-
-//debug($department_details);
-
-if (!$department_details) {
+if ($branch_details) {
 	$status = 'failed';
-	$_SESSION['errors']['department'] = "Department $_POST['department'] Already Exists!";
-} else {
-	header("location:../department-list.php?status=$status");
-	exit;
+	$_SESSION['errors']['branch'] = "Error! Branch already exists!";
 
-}
-
-$branch_details = $branch->getBranchByBranchname($_POST['branch'] , $_POST['department_id'] ) ; 
-debug($branch_details);
-
-if (!$branch_details) {
-		$status = 'failed';
-		$_SESSION['errors']['branch'] = "Post Failed! Branch ";
-	} else {
-		if (isset($_POST['action'])) {
+	if (isset($_POST['action'])) {
 		header("location:../branch.php?status=failed&action=" . $_POST['action']);
 	} else {
 		header("location:../branch.php?status=failed");
 	}
 	exit;
 
-	}
-
-
-if (isset($_POST['id']) && isset($_POST['action'])) {
-
-	//$branch_details = $branch->getBranchById($_POST['id']);
-
-	//debug($branch_details);
-
-	$branch->setData('branch', $_POST['branch']);
-	$branch->setData('department_id', $_POST['department_id']);
-
-	$result = $branch->update($_POST['id']);
-
-	//debug($result);
-
-	if (!$result) {
-		$status = 'failed';
-		$_SESSION['errors']['branch'] = "Update Failed!";
-	}
-
 } else {
 
-	$branch->setData('branch', $_POST['branch']);
-	$branch->setData('department_id', $_POST['department_id']);
+	if (isset($_POST['id']) && isset($_POST['action'])) {
 
-	$result = $branch->create();
+		$branch_details = $branch->getBranchById($_POST['id']);
 
-	//debug($result);
+		//debug($branch_details);
 
-	if (!$result) {
-		$status = 'failed';
-		$_SESSION['errors']['branch'] = "Post Failed!";
+		$branch->setData('branch', $_POST['branch']);
+		$branch->setData('department_id', $_POST['department_id']);
+
+		$result = $branch->update($_POST['id']);
+
+		//debug($result);
+
+		if (!$result) {
+			$status = 'failed';
+			$_SESSION['errors']['branch'] = "Update Failed!";
+		}
+
 	} else {
-		header("location:../branch-list.php?status=$status");
-		exit;
 
+		$branch->setData('branch', $_POST['branch']);
+		$branch->setData('department_id', $_POST['department_id']);
+
+		$result = $branch->create();
+
+		//debug($result);
+
+		if (!$result) {
+			$status = 'failed';
+			$_SESSION['errors']['branch'] = "Post Failed!";
+		} else {
+			header("location:../branch-list.php?status=$status");
+			exit;
+
+		}
 	}
+
 }
 
 header("location:../branch.php?id=" . $_POST['id'] . "&status=$status");
